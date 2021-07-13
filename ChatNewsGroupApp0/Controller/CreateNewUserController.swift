e3763491cf2d389a53135f75f96fc0f96f8ad4e9
github airy13-skyhi <airy13@icloud.com>
2021-07-13 19:17:24 +0900

    sendDBModel ADD

--- ChatNewsGroupApp0/Controller/CreateNewUserController.swift
+++ ChatNewsGroupApp0/Controller/CreateNewUserController.swift
@@ -10,8 +10,7 @@ import Firebase
 
 
 
-class CreateNewUserController: UIViewController {
-    
+class CreateNewUserController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     
     
@@ -21,15 +20,67 @@ class CreateNewUserController: UIViewController {
     
     
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-
+        
         
     }
     
-
+    
+    @IBAction func tapImageView(_ sender: Any) {
+        
+        //カメラ起動
+        openCamera()
+        
+        
+    }
+    
+    
+    
+    func openCamera(){
+        let sourceType:UIImagePickerController.SourceType = .photoLibrary
+        // カメラが利用可能かチェック
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            // インスタンスの作成
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            cameraPicker.allowsEditing = true
+            present(cameraPicker, animated: true, completion: nil)
+            
+        }else{
+            
+        }
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        
+        if let pickedImage = info[.editedImage] as? UIImage
+        {
+            profileImageView.image = pickedImage
+            //閉じる処理
+            picker.dismiss(animated: true, completion: nil)
+        }
+        
+    }
+    
+    // 撮影がキャンセルされた時に呼ばれる
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func createNewUser(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
     
 }
---
+++ ChatNewsGroupApp0/Model/SendDBModel.swift
@@ -0,0 +1,15 @@
+//
+//  SendDBModel.swift
+//  ChatNewsGroupApp0
+//
+//  Created by Manabu Kuramochi on 2021/07/13.
+//
+
+import Foundation
+
+
+class sendDBModel {
+    
+    
+}
+
