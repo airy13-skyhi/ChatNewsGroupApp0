82543255a6391fdad334949adbb4e7d2195f69f0
github airy13-skyhi <airy13@icloud.com>
2021-07-16 13:48:47 +0900

    NewsContents ADD

--- ChatNewsGroupApp0/Controller/NewsViewController.swift
+++ ChatNewsGroupApp0/Controller/NewsViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 
 class NewsViewController: UIViewController {
     
-    
+    var tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ ChatNewsGroupApp0/Model/NewsContents.swift
@@ -0,0 +1,17 @@
+//
+//  NewsContents.swift
+//  ChatNewsGroupApp0
+//
+//  Created by Manabu Kuramochi on 2021/07/16.
+//
+
+import Foundation
+
+
+struct NewsContents {
+    
+    
+    
+}
+
+
